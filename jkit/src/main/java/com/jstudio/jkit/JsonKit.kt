17ec93bb3e6d495e8c4d2fcd19b41904a6b020c4
jason <jason-lee@outlook.com>
2021-01-13 19:08:17 +0800

    jkit 0.0.10     JsonKit generic type loadinglayout 0.0.3

--- jkit/src/main/java/com/jstudio/jkit/JsonKit.kt
+++ jkit/src/main/java/com/jstudio/jkit/JsonKit.kt
@@ -6,6 +6,8 @@ import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.google.gson.JsonParser
 import com.google.gson.JsonSyntaxException
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 
 /**
  * Created by Jason
@@ -25,9 +27,18 @@ fun parseToJson(obj: Any): String = instance.toJson(obj)
 inline fun <reified T> parseToList(json: String): ArrayList<T> = parseToList(JsonParser.parseString(json))
 
 @Throws(JsonSyntaxException::class)
+inline fun <reified T> parseToList(json: String, itemClass: Class<T>): ArrayList<T> = instance.fromJson(json, ParameterizedTypeImpl(itemClass))
+
+@Throws(JsonSyntaxException::class)
 inline fun <reified T> parseToList(element: JsonElement): ArrayList<T> {
     val array = element.asJsonArray
     val list: ArrayList<T> = ArrayList()
     array.forEach { list.add(parseToObject(it)) }
     return list
+}
+
+class ParameterizedTypeImpl(var clazz: Class<*>) : ParameterizedType {
+    override fun getActualTypeArguments(): Array<Type> = arrayOf(clazz)
+    override fun getRawType(): Type = MutableList::class.java
+    override fun getOwnerType(): Type? = null
 }
